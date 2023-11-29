69fae2eb5f622b2ddf8f00fefbfd751a4a8ec704
Atri Thakar <atrithaker122@gmail.com>
2023-11-30 00:46:25 +0530

    All keywords done

---
+++ trail.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "gujju.h"
+
+int mukhya () {
+    printf("Namaskar, duniya!\n");
+    maate(purnaank i = 0; i < 10; i++)
+    {
+        jo(i%2==0)
+        chhapo("%d\n",i);
+    }
+    parat_karo 0;
+}
